a9ceb9377cf548ed63dc61a176b9df28f93ac1cc
Kantapon Dechjarernsi <59431606+KantaponDech@users.noreply.github.com>
2021-05-11 22:33:37 +0700

    Update main.R

--- main.R
+++ main.R
@@ -5,8 +5,8 @@ start.times <- Sys.time() ## Starting Time
 # library(DescTools)
 # library(ggplot2)
 
-source("C:/Users/student/Desktop/Final Project/import.R")
-setwd("C:/Users/student/Desktop/Final Project")
+source("../import.R")
+setwd("../Project")
 
 ## Property Setting
 N = 10000
@@ -25,18 +25,14 @@ for(skewed in skewed_){ ## Loop by skewness of Y
      ranY <- 0.1
      repeat{
           rr <- runif(1, 0.8, 1)
-          DataSet <- mvlognormal(n = N, Mu = c(7, 70000),
-                             Sigma = c(15, 5 ^ ranY),
-                             R = matrix(c(1,rr,rr,1), ncol = 2))
+          DataSet <- mvlognormal(n = N, Mu = c(7, 70000), Sigma = c(15, 5 ^ ranY), R = matrix(c(1,rr,rr,1), ncol = 2))
           X <- DataSet[,1]
           Y <- DataSet[,2]
           
-          if(!is.na(skewness(Y)) & cor(X,Y) >= 0.8 & cor(X,Y) <= 0.85
-             & skewness(Y) >= L_skew & skewness(Y) < R_skew)
-          {
-                setwd("C:/Users/student/Desktop/Final Project/Plot")
+          if(!is.na(skewness(Y)) & cor(X,Y) >= 0.8 & cor(X,Y) <= 0.85 & skewness(Y) >= L_skew & skewness(Y) < R_skew){
+                setwd("../Project/Plot")
                 plot_dataset()
-                setwd("C:/Users/student/Desktop/Final Project")
+                setwd("../Project")
                 break
           }
           
@@ -50,21 +46,15 @@ for(skewed in skewed_){ ## Loop by skewness of Y
      ## End of Simulate Population data
      
      ## Population Detail
-     data_detail <- rbind(median(X), median(Y), mean(X), mean(Y),
-                          sd(X), sd(Y), kurtosis(X), kurtosis(Y),
-                          skewness(X), skewness(Y), cor(X,Y))
+     data_detail <- rbind(median(X), median(Y), mean(X), mean(Y), sd(X), sd(Y), kurtosis(X), kurtosis(Y), skewness(X), skewness(Y), cor(X,Y))
      
-     row.names(data_detail) <- c("Median X", "Median Y", "Mean X", "Mean Y",
-                                 "SD X", "SD Y", "Kurtosis X", "Kurtosis Y",
-                                 "Skewness X", "Skewness Y", "Cor")
+     row.names(data_detail) <- c("Median X", "Median Y", "Mean X", "Mean Y", "SD X", "SD Y", "Kurtosis X", "Kurtosis Y", "Skewness X", "Skewness Y", "Cor")
      
-     write.table(data_detail, sep = ",", append = T,
-                 file = "C:/Users/student/Desktop/Final Project/Detail.csv")
+     write.table(data_detail, sep = ",", append = T, file = "../Project/Detail.csv")
      ## End of Population Detail
      
      ## Sampling and Estimation
-     for(sampleSize in n_)## Loop by sample size
-     {
+     for(sampleSize in n_){## Loop by sample size
          store <- numeric()
          store_Trad_Est    <- numeric()
          store_SisoDwive   <- numeric()
@@ -76,8 +66,7 @@ for(skewed in skewed_){ ## Loop by skewness of Y
          store_JerKis      <- numeric()
          store_MuiliAudu   <- numeric()
          
-         for (i in c(1:2000))
-         {
+         for (i in c(1:2000)){
             x <- numeric()
             y <- numeric()
             
@@ -103,9 +92,7 @@ for(skewed in skewed_){ ## Loop by skewness of Y
          
          ## Setting output
          ## List of estimated value
-         store <- list(store_Trad_Est, store_SisoDwive, store_SinghKa,
-                  store_UpaSingh, store_SinghTailor, store_YanTian,
-                  store_SubKum, store_JerKis, store_MuiliAudu)
+         store <- list(store_Trad_Est, store_SisoDwive, store_SinghKa, store_UpaSingh, store_SinghTailor, store_YanTian, store_SubKum, store_JerKis, store_MuiliAudu)
          
          MAE <- sapply(lapply(store, abs_dist), mean)            ## MAE
          RMSE <- sqrt(sapply(lapply(store, sqrt_dist), mean))    ## RMSE
@@ -115,8 +102,7 @@ for(skewed in skewed_){ ## Loop by skewness of Y
          row.names(result) <- estimator
          names(result) <- c("MAE", "RMSE")
          
-         write.table(result, sep = ",", append = T,
-                    file = "C:/Users/student/Desktop/Final Project/Result.csv")
+         write.table(result, sep = ",", append = T, file = "../Project/Result.csv")
          
          MAEs <- rbind(MAEs, MAE)
          RMSEs <- rbind(RMSEs, RMSE)
@@ -127,7 +113,7 @@ for(skewed in skewed_){ ## Loop by skewness of Y
 }
 
 ## error plot
-setwd("C:/Users/student/Desktop/Final Project/Plot")
+setwd("../Project/Plot")
 
 mae_n10 <- data.frame(rbind(MAEs[c(1,5,9,13),]))
 mae_n30 <- data.frame(rbind(MAEs[c(2,6,10,14),]))
@@ -144,7 +130,7 @@ rmse_n <- list(rmse_n10, rmse_n30, rmse_n100, rmse_n400)
 plot_error(mae_n, "MAE ")
 plot_error(rmse_n, "RMSE ")
 
-setwd("C:/Users/student/Desktop/Final Project")
+setwd("../Project")
 
 end.times <- Sys.time()
 runningTime <- end.times - start.times
