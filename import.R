42f406137aa4f402448f4667e8737dd4b76b5a7b
Kantapon Dechjarernsi <59431606+KantaponDech@users.noreply.github.com>
2021-05-11 22:34:38 +0700

    Update import.R

--- import.R
+++ import.R
@@ -4,9 +4,8 @@ MAEs <- numeric()
 RMSEs <- numeric()
 MAEn <- numeric()
 RMSEn <- numeric()
-estimator <- c("Traditional", "Sisodia and Dwivesdi", "Singh and Kakran",
-               "Upadhyaya and Singh", "Singh and Tailor", "Yan and Tian",
-               "Subramani and Kumarpandiyan", "Jerajuddin and Kishun", "Muili and Audu")
+estimator <- c("Traditional", "Sisodia and Dwivesdi", "Singh and Kakran", "Upadhyaya and Singh",
+               "Singh and Tailor", "Yan and Tian", "Subramani and Kumarpandiyan", "Jerajuddin and Kishun", "Muili and Audu")
 
 ## Approximate Function
 Trad_Est <- function(){ ## Traditional Estimator
@@ -51,8 +50,7 @@ plot_dataset <- function(){
     plt = ggplot(map = aes(X,Y, color = "red")) +
       geom_point() +
       geom_smooth(method = "lm", color = "chartreuse4") +
-      theme(plot.title = element_text(hjust = 0.5),
-            legend.position = "none", text = element_text(size = 28),
+      theme(plot.title = element_text(hjust = 0.5), legend.position = "none", text = element_text(size = 28),
             panel.border = element_rect(colour = "black", fill = NA, size = 1)) +
       labs(title = paste0("Dataset ", data_pic,
                           " with skewness of Y = ", skewed))
@@ -62,8 +60,7 @@ plot_dataset <- function(){
     png(filename = paste0("Density X", data_pic, ".png"), width = 840, height = 640)
     plt = ggplot(map = aes(X)) +
       geom_density(map = aes(y = ..scaled..), fill = "green", alpha = 0.5) +
-      theme(plot.title = element_text(hjust = 0.5),
-            legend.position = "none", text = element_text(size = 20),
+      theme(plot.title = element_text(hjust = 0.5), legend.position = "none", text = element_text(size = 20),
             panel.border = element_rect(colour = "black", fill = NA, size = 1)) +
       labs(title = paste0("Density of X in Dataset ", data_pic,
                           " with skewness of Y = ", skewed))
@@ -73,8 +70,7 @@ plot_dataset <- function(){
     png(filename = paste0("Density Y", data_pic, ".png"), width = 840, height = 640)
     plt = ggplot(map = aes(Y)) +
       geom_density(map = aes(y = ..scaled..), fill = "blue", alpha = 0.5) +
-      theme(plot.title = element_text(hjust = 0.5),
-            legend.position = "none", text = element_text(size = 20),
+      theme(plot.title = element_text(hjust = 0.5), legend.position = "none", text = element_text(size = 20),
             panel.border = element_rect(colour = "black", fill = NA, size = 1)) +
       labs(title = paste0("Density of Y in Dataset ", data_pic,
                           " with skewness of Y = ", skewed))
@@ -94,8 +90,7 @@ plot_error <- function(error, err_name){
         plt = ggplot(dat, aes(x = skw, y = val, group = grp, colour = grp)) +
             geom_line(size = 1) +
             theme(plot.title = element_text(hjust = 0.5), text = element_text(size = 28),
-                  legend.background = element_rect(fill = "lightblue", size = 1,
-                                                   linetype = "solid", colour = "darkblue"),
+                  legend.background = element_rect(fill = "lightblue", size = 1, linetype = "solid", colour = "darkblue"),
                   panel.border = element_rect(colour = "black", fill = NA, size = 1)) +
             labs(title = paste0(err_name, i), x = "Skewness", y = err_name) +
             scale_x_continuous(breaks = c(1,3,5,7)) +
